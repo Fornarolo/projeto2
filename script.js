72879267fdbc73cf1b4a5addef6c729988e3b408
Maria Rafaela <107640875+Fornarolo@users.noreply.github.com>
2023-05-08 21:11:12 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,25 @@
+function carregar() {
+    var msg = window.document.getElementById('msg')
+    var img = window.document.getElementById('imagem')
+    var data = new Date()
+    var hora = data.getHours()
+    var minuto = data.getMinutes();
+   //document.getElementById('msg').innerHTML = msgString;
+    msg.innerHTML = `Agora são ${hora} hora(s) e  ${minuto} minuto(s)` 
+    if(hora >= 0 && hora < 12) {
+       img.src = 'manha.jpg'
+       document.body.style.background = '#eaae8a'
+       men.innerHTML = "Bom dia!"
+    }
+    else if (hora >= 12 && hora < 18){
+       img.src = 'tarde.jpg'
+       document.body.style.background = '#ffc51b'
+       men.innerHTML = "Boa tarde!"
+    }
+    else {
+       img.src = 'noite.jpg'
+       document.body.style.background = '#382d4d'
+       men.innerHTML = "Boa noite!"
+    }
+
+}
